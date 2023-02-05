1c86760b4e84e959b26a169125a23d0590f2356f
Dércio  Matsimbe <123736124+DercioMatsimbe@users.noreply.github.com>
2023-02-05 16:26:15 -0300

    commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+
+
+
+var typed = new Typed(".typing", {
+    strings:["web Designer","web Developer", "Garph Designer", "Photographer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
